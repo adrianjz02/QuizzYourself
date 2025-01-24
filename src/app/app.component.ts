38d68785c615fb3e6e1c1dda93d2498d361988d3
Adrian <adr.jimenez.ian@gmail.com>
2025-01-24 16:11:55 +0100

    navbar update, cannot click on profile is user (modal appears if user cannot) is not logged in, add boostrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './shared/header/header.component';
 import {FooterComponent} from './shared/footer/footer.component';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'QuizzYourself';
+  //margin top pour ne pas superposer le header avec le contenu de la page
+  headerHeight: number = 0;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    const header = this.elementRef.nativeElement.querySelector('.navbar');
+    if (header) {
+      this.headerHeight = header.offsetHeight;
+    }
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal, TemplateRef, WritableSignal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AuthService} from '../../auth/services/auth.service';
 import {NgIf} from '@angular/common';
+import {NgbDatepickerModule, NgbOffcanvas, OffcanvasDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {Modal} from 'bootstrap';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
-    NgIf
+    NgIf,
+    NgbDatepickerModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
@@ -15,7 +19,8 @@ import {NgIf} from '@angular/common';
 export class HeaderComponent {
   isLoggedIn = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.authService.isLoggedIn().subscribe((status) => {
@@ -26,4 +31,45 @@ export class HeaderComponent {
   onLogout(): void {
     this.authService.logout(); // Déconnexion via le service
   }
+
+  // Pour le offcanva
+  private offcanvasService = inject(NgbOffcanvas);
+  closeResult: WritableSignal<string> = signal('');
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, {ariaLabelledBy: 'offcanvas-basic-title'}).result.then(
+      (result) => {
+        this.closeResult.set(`Closed with: ${result}`);
+      },
+      (reason) => {
+        this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+      },
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case OffcanvasDismissReasons.ESC:
+        return 'by pressing ESC';
+      case OffcanvasDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on the backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
+
+  onProfileClick(event: Event): void {
+    if (!this.isLoggedIn) {
+      event.preventDefault(); // Empêche la navigation
+      const modalElement = document.getElementById('exampleModal');
+      if (modalElement) {
+        const modal = new Modal(modalElement); // Crée une instance du modal
+        modal.show(); // Affiche le modal
+      }
+    } else {
+      // Redirige uniquement si l'utilisateur est connecté
+      this.router.navigate(['/user/profile']);
+    }
+  }
+
 }
