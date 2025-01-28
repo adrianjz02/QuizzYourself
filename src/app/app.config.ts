780707725ec470aa83fce0a19b2296f510072040
Adrian <adr.jimenez.ian@gmail.com>
2025-01-28 14:51:18 +0100

    footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import {provideHttpClient} from '@angular/common/http';
 import {provideCharts, withDefaultRegisterables} from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideCharts(withDefaultRegisterables()), provideCharts(withDefaultRegisterables())]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideCharts(withDefaultRegisterables()), provideCharts(withDefaultRegisterables()), provideAnimationsAsync()]
 };
